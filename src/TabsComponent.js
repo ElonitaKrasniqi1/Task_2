3b996d99eb27ff73e2abc4d39c4f68175371c638
Elonita Krasniqi <elonitakrasniqi@gmail.com>
2024-02-17 15:05:44 +0100

    Cleaner version!

--- src/TabsComponent.js
+++ src/TabsComponent.js
@@ -5,74 +5,32 @@ import CardContainer from "./CardContainer";
 import "./TabsComponent.css";
 
 export default function TabsComponent() {
-  const [menuVisible, setMenuVisible] = useState(false);
   const [selectedTabIndex, setSelectedTabIndex] = useState(0);
-
-  useEffect(() => {
-    const savedTabIndex = localStorage.getItem("selectedTabIndex");
-    if (savedTabIndex !== null) {
-      setSelectedTabIndex(parseInt(savedTabIndex, 10));
-    }
-  }, []);
-
-  const toggleMenu = () => {
-    setMenuVisible(!menuVisible);
-    if (!menuVisible) {
-      setSelectedTabIndex(0);
-    }
-  };
 
   const handleTabSelect = (index) => {
     setSelectedTabIndex(index);
-    localStorage.setItem("selectedTabIndex", index.toString());
   };
 
   return (
-    <div className={`tabs-container ${menuVisible ? "menu-visible" : ""}`}>
-      <button className="menu-toggle" onClick={toggleMenu}>
-        ☰
-      </button>
-      <Tabs
-        className={`tab-container ${menuVisible ? "menu-visible" : "menu-hidden"}`}
-        selectedIndex={selectedTabIndex}
-        onSelect={handleTabSelect}
-      >
+    <div className="tabs-container">
+      <Tabs selectedIndex={selectedTabIndex} onSelect={handleTabSelect}>
         <TabList className="tab-list">
-       <Tab className="tab"><span className="link">Domains</span></Tab>
-        <Tab className="tab"><span className="link">Web Hosting</span></Tab>
-        <Tab className="tab"><span className="link">Dedicated Servers</span></Tab>
-        <Tab className="tab"><span className="link">Virtual Cloud Servers</span></Tab>
-        <Tab className="tab"><span className="link">WordPress Hosting</span></Tab>
-        <Tab className="tab"><span className="link">Email Hosting</span></Tab>
-        <Tab className="tab"><span className="link">VPS Hosting Servers</span></Tab>
-        <Tab className="tab"><span className="link">Free Hosting</span></Tab>
-      </TabList>
-  
-      <TabPanel className={`menu__box ${menuVisible ? "open" : ""}`}>
-        <CardContainer domainSet={1} />
-      </TabPanel>
-      <TabPanel>
-        <CardContainer domainSet={2} />
-      </TabPanel>
-      <TabPanel>
-        <CardContainer domainSet={3} />
-      </TabPanel>
-      <TabPanel>
-        <CardContainer domainSet={4} />
-      </TabPanel>
-      <TabPanel>
-        <CardContainer domainSet={5} />
-      </TabPanel>
-      <TabPanel>
-        <CardContainer domainSet={6} />
-      </TabPanel>
-      <TabPanel>
-        <CardContainer domainSet={7} />
-      </TabPanel>
-      <TabPanel>
-        <CardContainer domainSet={8} />
-      </TabPanel>
-    </Tabs>
+          <Tab className="tab"><span className="link">Domains</span></Tab>
+          <Tab className="tab"><span className="link">Web Hosting</span></Tab>
+          <Tab className="tab"><span className="link">Dedicated Servers</span></Tab>
+          <Tab className="tab"><span className="link">Virtual Cloud Servers</span></Tab>
+          <Tab className="tab"><span className="link">WordPress Hosting</span></Tab>
+          <Tab className="tab"><span className="link">Email Hosting</span></Tab>
+          <Tab className="tab"><span className="link">VPS Hosting Servers</span></Tab>
+          <Tab className="tab"><span className="link">Free Hosting</span></Tab>
+        </TabList>
+
+        {Array.from({ length: 8 }).map((_, index) => (
+          <TabPanel key={index}>
+            <CardContainer domainSet={index + 1} />
+          </TabPanel>
+        ))}
+      </Tabs>
     </div>
   );
 }
